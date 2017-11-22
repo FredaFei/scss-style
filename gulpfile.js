d8573ea7bc0ea8d1328d31d99bc26bd2a0f0b075
fei-han <964959033@qq.com>
2017-11-23 00:05:44 +0800

    radio style

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+
+gulp.task('sass',function(){
+    return gulp.src('./scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css/'))
+})
+
+// 默认任务
+gulp.task('default',['sass'], function(){
+    // gulp.run('sass');
+});
